5ef9be8245c7b2f6c1281b78682a829388bcfd01
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-28 18:10:42 -0700

    export all functions for each section of web portfolio to preview page

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -1,1 +1,25 @@
 import React from 'react';
+const styles = {
+    card: {
+      margin: 20,
+      background: '#e8eaf6',
+    },
+    heading: {
+      background: '#9a74db',
+      minHeight: 50,
+      lineHeight: 3.5,
+      fontSize: '1.2rem',
+      color: 'white',
+      padding: '0 20px',
+    },
+  };
+
+function Portfolio() {
+    return (
+      <div style={styles.card}>
+        <div style={styles.heading}>Home</div>
+      </div>
+    );
+  }
+
+export default Portfolio;
