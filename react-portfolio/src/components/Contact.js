fa5d7736e7385f2bbe57f048eecf9e249966855e
Kylie Shinn <kyliemshinn@gmail.com>
2022-05-28 20:43:37 -0700

    add contact form to the bottom of the page- no styling yet

--- react-portfolio/src/components/Contact.js
+++ react-portfolio/src/components/Contact.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React from "react";
 
 const styles = {
-    card: {
-      margin: 20,
-      background: '#e8eaf6',
-    },
-    heading: {
-      background: '#9a74db',
-      minHeight: 50,
-      lineHeight: 3.5,
-      fontSize: '1.2rem',
-      color: 'white',
-      padding: '0 20px',
-    },
-  };
+  card: {
+    margin: 20,
+    padding: 10,
+    background: "#e8eaf6",
+    width: 500,
+    justifyContent: 'center',
+    alignItem: 'center'
+  },
+  form: {
+    justifyContent: "center"
+  }
+};
 
 function Contact() {
-    return (
-      <div style={styles.card}>
-        <div style={styles.heading}>Home</div>
-      </div>
-    );
-  }
+  return (
+    <div style={styles.card}>
+      <form style={styles.form}>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">
+            Name
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+            placeholder="Person Who Wants To Hire Me"
+          ></input>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="hireme@gmail.com"
+          ></input>
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlTextarea1" class="form-label">
+            Example textarea
+          </label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>{" "}
+        <button type="submit" class="btn btn-danger">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
 
 export default Contact;
